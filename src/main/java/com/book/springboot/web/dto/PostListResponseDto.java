c93ef729437df2af594804e89a201ead98b7d481
송대훈 <musang33@nate.com>
2021-09-19 19:49:54 +0900

    complete web application

---
+++ src/main/java/com/book/springboot/web/dto/PostListResponseDto.java
@@ -0,0 +1,21 @@
+package com.book.springboot.web.dto;
+
+import com.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDateTime;
+
+    public PostListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDateTime = entity.getModifiedDate();
+    }
+}
